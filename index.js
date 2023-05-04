f761d833f414b76851c86583b48eca6944cc19d1
Topu Roy <topuroy143333@gmail.com.com>
2023-05-04 22:44:04 +0600

    Added Authentication via cookie and added form data to the Database MongoDB Compass

--- index.js
+++ index.js
@@ -2,27 +2,68 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = 5002;
 
+//* Connect to database
+mongoose
+  .connect("mongodb://127.0.0.1:27017", { dbName: "users" })
+  .then(() => console.log("Database connected !"));
+
+//* Mongoose Schema
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+});
+
+const userModel = mongoose.model("user", userSchema);
+
+//* Middleware
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.use(cookieParser());
+
+//* Handlers
+const isUserAuthenticated = (req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    next();
+  } else {
+    res.render("login-form");
+  }
+};
+
+const addUserToDataBaseAndCreateCookie = async (req, res, next) => {
+  const { name, email } = req.body;
+  const user = await userModel.create({ name, email });
+
+  //* Cookie
+  res.cookie("token", user._id, {
+    expires: new Date(Date.now() + 300 * 1000),
+  });
+  next();
+};
 
 //* Access Body Parser Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.render("login-form");
-});
-
-app.post("/login", (req, res) => {
-  const { name, email } = req.body;
-  console.log(name, email);
+//* Routes
+app.get("/", isUserAuthenticated, (req, res) => {
   res.render("index");
 });
-app.get("/logout", (req, res) => {});
 
-app.listen(port, () => console.log(`server listening on ${port}`));
+app.post("/login", addUserToDataBaseAndCreateCookie, (req, res) => {
+  res.render("index");
+});
+
+app.get("/logout", (req, res) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+  });
+  res.redirect("/");
+});
+
+app.listen(port, () => console.log(`server listening on ${port} !`));
