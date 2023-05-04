0eb3224bdcb67f06284c1bc719348af2eec22ba9
Topu Roy <topuroy143333@gmail.com.com>
2023-05-05 01:16:52 +0600

    Added JWT Token and Added Display name , email on the index.ejs page

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 const port = 5002;
@@ -18,7 +19,7 @@ const userSchema = new mongoose.Schema({
   email: String,
 });
 
-const userModel = mongoose.model("user", userSchema);
+const User = mongoose.model("user", userSchema);
 
 //* Middleware
 app.use(express.static(path.join(__dirname, "public")));
@@ -26,9 +27,11 @@ app.set("view engine", "ejs");
 app.use(cookieParser());
 
 //* Handlers
-const isUserAuthenticated = (req, res, next) => {
+const isUserAuthenticated = async (req, res, next) => {
   const { token } = req.cookies;
   if (token) {
+    const decodedUserToken = jwt.verify(token, "secret");
+    req.user = await User.findById(decodedUserToken._id);
     next();
   } else {
     res.render("login-form");
@@ -37,10 +40,13 @@ const isUserAuthenticated = (req, res, next) => {
 
 const addUserToDataBaseAndCreateCookie = async (req, res, next) => {
   const { name, email } = req.body;
-  const user = await userModel.create({ name, email });
+  const user = await User.create({ name, email });
+
+  //* JWT token
+  const token = jwt.sign({ _id: user._id }, "secret");
 
   //* Cookie
-  res.cookie("token", user._id, {
+  res.cookie("token", token, {
     expires: new Date(Date.now() + 300 * 1000),
   });
   next();
@@ -52,11 +58,11 @@ app.use(bodyParser.json());
 
 //* Routes
 app.get("/", isUserAuthenticated, (req, res) => {
-  res.render("index");
+  res.render("index", { name: req.user.name, email: req.user.email });
 });
 
 app.post("/login", addUserToDataBaseAndCreateCookie, (req, res) => {
-  res.render("index");
+  res.redirect("/");
 });
 
 app.get("/logout", (req, res) => {
